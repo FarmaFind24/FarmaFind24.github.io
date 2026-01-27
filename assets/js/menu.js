5824e7ac6e268f180ec984be5a981c5bd1db293f
Scafu <alessandro.mazzariol04@gmail.com>
2026-01-27 22:41:55 +0100

    fix navbar

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.querySelector(".menu-toggle");
   const toggle2 = document.querySelector(".menu-toggle-text");
-  const menu = document.querySelector(".Top-NavBar ul");
+  const menu = document.querySelector(".top-navbar ul");
 
   if (!toggle || !toggle || !menu) return;
 
   toggle.addEventListener("click", () => {
-    const isOpen = menu.classList.toggle("open"); 
+    const isOpen = menu.classList.toggle("open");
     toggle.setAttribute("aria-expanded", isOpen);
     toggle2.setAttribute("aria-expanded", isOpen);
   });
